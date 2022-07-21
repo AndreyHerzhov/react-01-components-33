50c284c6002c04a66e2f8ca57d80a745be371833
AndreyHerzhov <andreyherzhov@gmail.com>
2022-07-21 15:51:10 +0300

    Event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
-import { PageTitle } from 'components/PageTitle/PageTitle';
-import { EventBoard } from 'components/EventBoard/EventBoard';
-import upcomingEvents from '../upcoming-events.json';
+import { PageTitle } from "./PageTitle/PageTitle";
+import { EventBoard } from "./EventBoard/EventBoard";
+import upcomingEvents from '../upcoming-events.json'
 
 export const App = () => {
-  return (
-    <>
-      <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventBoard events={upcomingEvents} />
-    </>
-  );
+  return <div>
+    <PageTitle text="24th Core Worlds Coalition Conference"/>
+    <EventBoard events={upcomingEvents}/>
+  </div>
 };
